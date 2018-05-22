a1479b8f65840174d7613ac7ec3ccb8d3b4172bf
martastaporek <staporekmarta1@gmail.com>
2018-05-22 16:19:09 +0200

    Implement algorithm

--- src/main/java/TinkerTailor.java
+++ src/main/java/TinkerTailor.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TinkerTailor {
+
+    private int N; //number of elements
+    private int K; // fold
+    private List <Integer> array = new ArrayList<>();
+
+    public TinkerTailor(int n, int k) {
+        this.N = n;
+        this.K = k;
+    }
+
+    public int getWinner(){
+        generateArray();
+        int index = 0;
+        while (this.array.size() > 1){
+            index = (index + this.K - 1) % this.array.size();
+            this.array.remove(this.array.get(index));
+        }
+        return this.array.get(0);
+
+    }
+
+    private void generateArray(){
+        for(int i =1; i <= this.N; i++){
+            array.add(i);
+        }
+    }
 }
